7b71702f3e36b13f486207e30934cbcc04a45003
hemanta212 <sharmahemanta.212@gmail.com>
2021-07-04 14:33:09 +0000

    Complete a side sign generation

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 import subprocess
 
+
 def main():
-    cmd = "manim render triangle_gen.py --quality h --silent -v ERROR -s"
-    if not os.path.exists('images'):
-        os.mkdir('images')
+    cmd = "manim render triangle.py --quality h --silent -v ERROR -s"
+    if not os.path.exists("images"):
+        os.mkdir("images")
 
     print("::Processing...")
-    subprocess.run(cmd, shell=True, text=True, stdout=subprocess.PIPE, check=True)
     print("::Reading triangle.ini...")
+    subprocess.run(cmd, shell=True, text=True, stdout=subprocess.PIPE, check=True)
     print("::Output Generated at: ./images folder")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
